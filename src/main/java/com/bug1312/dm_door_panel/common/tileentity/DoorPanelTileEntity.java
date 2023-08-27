79e66c1477c1a119667e706afd7cfe80a607cbe5
Bug1312 <bug1312.babycarrots1312@gmail.com>
2023-08-27 10:48:45 -0400

    Squashed commit for v1.0.2

--- src/main/java/com/bug1312/dm_door_panel/common/tileentity/DoorPanelTileEntity.java
+++ src/main/java/com/bug1312/dm_door_panel/common/tileentity/DoorPanelTileEntity.java
@@ -4,6 +4,7 @@ package com.bug1312.dm_door_panel.common.tileentity;
 
 import com.bug1312.dm_door_panel.common.block.DoorPanelBlock;
 import com.bug1312.dm_door_panel.common.init.BlockEntities;
+import com.swdteam.common.init.DMDimensions;
 import com.swdteam.common.init.DMTardis;
 import com.swdteam.common.init.DMTranslationKeys;
 import com.swdteam.common.tardis.Location;
@@ -32,65 +33,74 @@ public class DoorPanelTileEntity extends DMTileEntityBase implements ITickableTi
 		tardis.setDoor(TardisDoor.BOTH, open, DoorSource.INTERIOR);
 	}
 	
-	
-	public boolean toggleDoors(PlayerEntity player) {
-		TardisData data = DMTardis.getTardisFromInteriorPos(this.getBlockPos());
-		if (data == null) return !this.getBlockState().getValue(DoorPanelBlock.OPENED);
+	public boolean toggleDoors(PlayerEntity player) {	
+		if (this.level.dimension() == DMDimensions.TARDIS) {		
+			TardisData data = DMTardis.getTardisFromInteriorPos(this.getBlockPos());
+			if (data != null) {				
+				if (data.isLocked()) {
+					player.displayClientMessage(new StringTextComponent(TextFormatting.YELLOW + DMTranslationKeys.TARDIS_IS_LOCKED.getString()), true);
+					return this.getBlockState().getValue(DoorPanelBlock.OPENED);
+				}
+				
+				if (data.isInFlight()) {
+					player.displayClientMessage(new StringTextComponent(TextFormatting.YELLOW + DMTranslationKeys.TARDIS_IN_FLIGHT.getString()), true);
+					return this.getBlockState().getValue(DoorPanelBlock.OPENED);
+				}
+				
+				Location location = data.getCurrentLocation();
+				ServerWorld serverWorld = level.getServer().getLevel(data.getCurrentLocation().dimensionWorldKey());
+				TileEntity tile = serverWorld.getBlockEntity(location.getPosition().toBlockPos());
 		
-		if (data.isLocked()) {
-			player.displayClientMessage(new StringTextComponent(TextFormatting.YELLOW + DMTranslationKeys.TARDIS_IS_LOCKED.getString()), true);
-			return this.getBlockState().getValue(DoorPanelBlock.OPENED);
+				if (tile != null && tile instanceof TardisTileEntity) {
+					TardisTileEntity tardis = (TardisTileEntity) tile;
+					boolean isOpen = tardis.doorOpenLeft || tardis.doorOpenRight;
+					TranslationTextComponent text = new TranslationTextComponent(isOpen ? "notice.dm_door_panel.close" : "notice.dm_door_panel.open");
+					
+					setDoors(tardis, !isOpen);
+					data.setDoorOpen(!isOpen);
+					player.displayClientMessage(new StringTextComponent(TextFormatting.GREEN + text.getString()), true);
+			
+					return !isOpen;
+				}
+			}
 		}
 		
-		if (data.isInFlight()) {
-			player.displayClientMessage(new StringTextComponent(TextFormatting.YELLOW + DMTranslationKeys.TARDIS_IN_FLIGHT.getString()), true);
-			return this.getBlockState().getValue(DoorPanelBlock.OPENED);
-		}
-		
-		Location location = data.getCurrentLocation();
-		ServerWorld serverWorld = level.getServer().getLevel(data.getCurrentLocation().dimensionWorldKey());
-		TileEntity tile = serverWorld.getBlockEntity(location.getPosition().toBlockPos());
-
-		if (tile == null || !(tile instanceof TardisTileEntity)) return this.getBlockState().getValue(DoorPanelBlock.OPENED);
-		
-		TardisTileEntity tardis = (TardisTileEntity) tile;
-		boolean isOpen = tardis.doorOpenLeft || tardis.doorOpenRight;
-		TranslationTextComponent text = new TranslationTextComponent(isOpen ? "notice.dm_door_panel.close" : "notice.dm_door_panel.open");
-		
-		setDoors(tardis, !isOpen);
-		data.setDoorOpen(!isOpen);
-		player.displayClientMessage(new StringTextComponent(TextFormatting.GREEN + text.getString()), true);
-
-		return !isOpen;
+		return !this.getBlockState().getValue(DoorPanelBlock.OPENED);
 	}
 	
 	public boolean toggleLocks(PlayerEntity player) {
-		TardisData data = DMTardis.getTardisFromInteriorPos(this.getBlockPos());
-		if (data == null) return !this.getBlockState().getValue(DoorPanelBlock.LOCKED);
-		data.setLocked(!data.isLocked());
-		
-		TranslationTextComponent text = new TranslationTextComponent(data.isLocked() ? "notice.dm_door_panel.lock" : "notice.dm_door_panel.unlock");
-		player.displayClientMessage(new StringTextComponent(TextFormatting.GREEN + text.getString()), true);
-		
-		if (data.isLocked()) {
-			Location location = data.getCurrentLocation();
-			ServerWorld serverWorld = level.getServer().getLevel(data.getCurrentLocation().dimensionWorldKey());
-			TileEntity tile = serverWorld.getBlockEntity(location.getPosition().toBlockPos());
-
-			if (tile != null && tile instanceof TardisTileEntity) {
-				TardisTileEntity tardis = (TardisTileEntity) tile;
+		if (this.level.dimension() == DMDimensions.TARDIS) {
+			TardisData data = DMTardis.getTardisFromInteriorPos(this.getBlockPos());
+			if (data != null) {
+				data.setLocked(!data.isLocked());
 				
-				if (tardis.doorOpenLeft || tardis.doorOpenRight) setDoors(tardis, false);
+				TranslationTextComponent text = new TranslationTextComponent(data.isLocked() ? "notice.dm_door_panel.lock" : "notice.dm_door_panel.unlock");
+				player.displayClientMessage(new StringTextComponent(TextFormatting.GREEN + text.getString()), true);
+				
+				if (data.isLocked()) {
+					Location location = data.getCurrentLocation();
+					ServerWorld serverWorld = level.getServer().getLevel(data.getCurrentLocation().dimensionWorldKey());
+					TileEntity tile = serverWorld.getBlockEntity(location.getPosition().toBlockPos());
+		
+					if (tile != null && tile instanceof TardisTileEntity) {
+						TardisTileEntity tardis = (TardisTileEntity) tile;
+						
+						if (tardis.doorOpenLeft || tardis.doorOpenRight) setDoors(tardis, false);
+					}
+					
+					data.setDoorOpen(false);
+				}
+		
+				return data.isLocked();
 			}
-			
-			data.setDoorOpen(false);
 		}
 
-		return data.isLocked();
+		return !this.getBlockState().getValue(DoorPanelBlock.LOCKED);
 	}
-
+		
 	@Override
 	public void tick() {
+		if (this.level.dimension() != DMDimensions.TARDIS) return;
 		TardisData data = DMTardis.getTardisFromInteriorPos(this.getBlockPos());
 		if (data == null) return;
 		
